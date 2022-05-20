8175c64c3910af00deda28e9bfe9758dbb0e8783
Uselessface <Uselessface@mail.ru>
2022-05-20 10:22:03 +0400

    refactor

--- my-app/src/components/Dialogs/DialogueBody/DialogBody.jsx
+++ my-app/src/components/Dialogs/DialogueBody/DialogBody.jsx
@@ -1,41 +1,16 @@
 import React from "react";
 import styles from "./Dialogue_body.module.css";
-import ProfileStyle from "../../Profile/ProfileInfo/ProfileInfo.module.css";
+import Message from "./Message";
+
+
 
 
 let messageData = [
     {id : 1, message : "Реально хороший и понятный курс. надеюсь это сказать и в конце."}
     ,{ id: 2, message: "Спасибо!" }
 ]
-let messageBody = messageData.map(content => <IncomingMessage message_content={content.message} /> );
+let messageBody = messageData.map(content => <Message message_content={content.message} /> );
 
-const IncomingMessage = (props) => {
-    return(
-        <div className={`${styles.message} ${styles.incoming_message}`}>
-            <div className={styles.userAvatar}>
-                <img src="https://99px.ru/sstorage/53/2020/10/mid_316610_252677.jpg" alt=""
-                     className={ProfileStyle.avatar_image}/>
-            </div>
-            <div className={styles.message_content}>
-                {props.message_content}
-            </div>
-        </div>
-    );
-}
-
-/*const ReplyingMessage = (props) => {
-    return(
-        <div className={`${styles.message} ${styles.reply_message}`}>
-            <div className={styles.userAvatar}>
-                <img src="https://99px.ru/sstorage/53/2020/10/mid_316610_252677.jpg" alt=""
-                     className={ProfileStyle.avatar_image}/>
-            </div>
-            <div className={styles.message_content}>
-                {props.message_content}
-            </div>
-        </div>
-    );
-}*/
 
 const DialogBody = (props) =>{
     return (
---
+++ my-app/src/components/Dialogs/DialogueBody/Message.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Dialogue_body.module.css";
+import ProfileStyle from "../../Profile/ProfileInfo/ProfileInfo.module.css";
+
+const Message = (props) => {
+    return(
+        <div className={`${styles.message} ${styles.incoming_message}`}>
+            <div className={styles.userAvatar}>
+                <img src="https://99px.ru/sstorage/53/2020/10/mid_316610_252677.jpg" alt=""
+                     className={ProfileStyle.avatar_image}/>
+            </div>
+            <div className={styles.message_content}>
+                {props.message_content}
+            </div>
+        </div>
+    );
+}
+
+export default Message;
